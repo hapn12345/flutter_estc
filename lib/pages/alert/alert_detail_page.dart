a268a695c43cbe70e36356f858cf90b0bde2a9a1
khaitq-rikkeisoft <khaitq@rikkeisoft.com>
2022-09-30 15:01:07 +0700

    -add animation and alert sound for alert page

--- lib/pages/alert/alert_detail_page.dart
+++ lib/pages/alert/alert_detail_page.dart
@@ -1,4 +1,11 @@
+import 'dart:async';
+
+import 'package:audioplayers/audioplayers.dart';
+import 'package:estc_project/util/log_util.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+typedef OnError = void Function(Exception exception);
 
 class AlertDetailPage extends StatefulWidget {
   final String alertId;
@@ -10,6 +17,26 @@ class AlertDetailPage extends StatefulWidget {
 }
 
 class _AlertDetailPageState extends State<AlertDetailPage> {
+  AudioPlayer player = AudioPlayer()..setReleaseMode(ReleaseMode.loop);
+
+  List<StreamSubscription> streams = [];
+
+  @override
+  void initState() {
+    super.initState();
+    streams.add(player.onPlayerComplete.listen((event) {
+      LogUtil.d('Player complete!');
+    }));
+    streams.add(player.onSeekComplete.listen((event) {
+      LogUtil.d('Seek complete!');
+    }));
+    preparePlayer();
+  }
+
+  Future<void> preparePlayer() async {
+    await player.play(AssetSource('alarm.mp3'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,8 +46,26 @@ class _AlertDetailPageState extends State<AlertDetailPage> {
       body: Center(
           child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: [Text('Title: ${widget.alertId}'), const Text('Description')],
+        children: [
+          Lottie.asset('assets/alert-exclamation.json'),
+          Text('Title: ${widget.alertId}'),
+          const Text('Description'),
+          ElevatedButton(
+              onPressed: () async {
+                await player.release();
+              },
+              child: const Text('Stop'))
+        ],
       )),
     );
+  }
+
+  @override
+  void dispose() {
+    for (var it in streams) {
+      it.cancel();
+    }
+    player.release();
+    super.dispose();
   }
 }
